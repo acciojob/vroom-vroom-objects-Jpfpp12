670f2033e08d6c0c9a179722479841f5c6492d8b
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-03-25 19:32:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// Complete the js code
 function Car(make, model) {
-	 this.make = make;
+    this.make = make;
     this.model = model;
 }
+
+// Add method to the Car prototype
 Car.prototype.getMakeModel = function() {
     return `${this.make} ${this.model}`;
 };
 
+// SportsCar Constructor (inherits from Car)
 function SportsCar(make, model, topSpeed) {
-	    Car.call(this, make, model);
+    // Call the parent constructor (Car)
+    Car.call(this, make, model);
     this.topSpeed = topSpeed;
 }
 
+// Inherit from Car's prototype
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
 
+// Add method to SportsCar prototype
 SportsCar.prototype.getTopSpeed = function() {
     return this.topSpeed;
 };
-}
-
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
